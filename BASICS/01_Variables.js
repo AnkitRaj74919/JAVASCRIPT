0f373decca42001bcf7e5fa49637ce3851c58ecd
AnkitRaj74919 <128829478+AnkitRaj74919@users.noreply.github.com>
2024-10-27 18:45:38 +0000

    Variables

---
+++ BASICS/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId=11223213
+let accountEmail="ankit74919@gmail.com"
+// var accountPassword="Motihari"
+accountCity="Jaipur"
+let accountState;
+
+accountEmail="abc23@gmail.com"
+accountPassword="324334"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
